2324537bc9adac98bf2b827dfc54650b67b80d3a
Abhishek Singla <abhishek@example.com>
2026-06-05 16:32:31 +0530

    Fix test package: move CostAndInterestTest to default package to match source

--- src/test/java/CostAndInterestTest.java
+++ src/test/java/CostAndInterestTest.java
@@ -1,9 +1,7 @@
-package epam;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
+class CostAndInterestTest {
 
     // ---- Cost / HouseConstruction tests ----
 
